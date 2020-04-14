37ee1b5729499422a63b371133b375868937c54f
Jure <jure.mrzek15@gmail.com>
2020-04-14 17:25:29 +0200

    Added a wall that doesn't do anything
    Added a movable wall. It can detect a collision but doesn't do anything yet.

--- javascript/Classes.js
+++ javascript/Classes.js
@@ -40,11 +40,11 @@ class Ball{
         ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.stroke();
     }
-    distanceFromRectangle(x, y, width, height){ //distance from ball and a rectangle
+    /*distanceFromRectangle(x, y, width, height){ //distance from ball and a rectangle
         let dx = Math.max(Math.abs(this.x - x) - width / 2, 0);
         let dy = Math.max(Math.abs(this.y - y) - height / 2, 0);
         return Math.sqrt(dx * dx + dy * dy);
-    }
+    }*/
     distanceFromPoint(x,y){
         //console.log(x+ " lčmao "+this.x);
         return Math.sqrt((this.x-x)*(this.x-x) + (this.y-y)*(this.y-y));
@@ -54,5 +54,43 @@ class Point{
     constructor(x, y){
         this.x = x;
         this.y = y;
+    }
+}
+class Wall{
+    constructor(startx, starty, endx, endy, radius){
+        this.startx = startx;
+        this.starty = starty;
+        this.endx = endx;
+        this.endy = endy;
+        this.r = radius;
+        this.startSelected = false;
+        this.endSelected = false;
+        this.length = Math.sqrt((this.startx-this.endx)*(this.startx-this.endx) + (this.starty-this.endy)*(this.starty-this.endy));
+    }
+    draw(fillColor, strokeColor){
+        ctx.fillStyle = fillColor;
+        ctx.beginPath();
+        ctx.arc(this.startx, this.starty, this.r, 0, 2 * Math.PI);
+        ctx.arc(this.endx, this.endy, this.r, 0, 2 * Math.PI);
+        ctx.fill();
+
+        this.length = Math.sqrt((this.startx-this.endx)*(this.startx-this.endx) + (this.starty-this.endy)*(this.starty-this.endy));
+        let xvector = (this.startx - this.endx)/this.length;
+        let yvector = (this.starty - this.endy)/this.length;
+        let xnormal = -yvector;
+        let ynormal = xvector;
+
+        ctx.strokeStyle = strokeColor;
+        let p1 = new Point(this.startx+xnormal*this.r, this.starty+ynormal*this.r);
+        let p2 = new Point(this.endx+xnormal*this.r, this.endy+ynormal*this.r);
+        ctx.beginPath();
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+
+        p1 = new Point(this.startx-xnormal*this.r, this.starty-ynormal*this.r);
+        p2 = new Point(this.endx-xnormal*this.r, this.endy-ynormal*this.r);
+        ctx.moveTo(p1.x, p1.y);
+        ctx.lineTo(p2.x, p2.y);
+        ctx.stroke();
     }
 }
--- javascript/Main.js
+++ javascript/Main.js
@@ -2,9 +2,9 @@ const canvas = document.getElementById("canvas");
 canvas.width = 1200;
 canvas.height = 800;
 const ctx = canvas.getContext("2d");
-const friction = 0.002; //from 0 to 1 - 
 let paddle = new Paddle(450,canvas.height-64,256,48,6);
 let balls = [];
+let walls = [];
 let numberOfBalls = 2;
 let mouse = new Point(0,0);
 let mouseDown = false;
@@ -16,18 +16,32 @@ balls.push(new Ball(130, 400, 40, 0, 0, 4));
 balls.push(new Ball(530, 300, 45, 0, 0, 5));
 balls.push(new Ball(780, 200, 55, 0, 0, 6));
 balls.push(new Ball(820, 600, 70, 0, 0, 7));
+
+walls.push(new Wall(200, 650, 400, 500, 30));
 mainLoop();
 function mainLoop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Drag walls
+    walls.forEach((wall) => {
+        if(wall.startSelected){
+            wall.startx = mouse.x;
+            wall.starty = mouse.y;
+        }
+        if(wall.endSelected){
+            wall.endx = mouse.x;
+            wall.endy = mouse.y;
+        }
+    });
+
     balls.forEach((ball) => {
-        if(mouseDown){
-            if(ball.selected){
-                ctx.beginPath();
-                ctx.strokeStyle = "blue";
-                ctx.moveTo(ball.x, ball.y);
-                ctx.lineTo(mouse.x, mouse.y);
-                ctx.stroke();
-            }
+        //Drag balls
+        if(ball.selected){
+            ctx.beginPath();
+            ctx.strokeStyle = "blue";
+            ctx.moveTo(ball.x, ball.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
         }
         ball.x += ball.vx;
         ball.y += ball.vy;
@@ -37,17 +51,24 @@ function mainLoop(){
         if(ball.y<=ball.r || ball.y>=canvas.height-ball.r)
             ball.vy = -ball.vy;
         
-        /* */
+        
+        //Collision
         for(let i=0; i<balls.length; i++){
             if(ball.id == i)
                 break;
-            if(ball.distanceFromPoint(balls[i].x, balls[i].y) <= ball.r+balls[i].r){ //check collision between two balls
-                let overlap = 0.5*(ball.distanceFromPoint(balls[i].x, balls[i].y) - ball.r - balls[i].r);
-                ball.x -= overlap * (ball.x - balls[i].x)/ball.distanceFromPoint(balls[i].x, balls[i].y); //razstavimo dejanski razmak krogov v x in y koordinato -
-                ball.y -= overlap * (ball.y - balls[i].y)/ball.distanceFromPoint(balls[i].x, balls[i].y); //če bo x=0.2 bo y=0.8, skupaj bo vedno 1. Potem pomnožimo z
-                balls[i].x += overlap * (ball.x - balls[i].x)/ball.distanceFromPoint(balls[i].x, balls[i].y); //razdaljo, za katero hočemo premakniti ball
-                balls[i].y += overlap * (ball.y - balls[i].y)/ball.distanceFromPoint(balls[i].x, balls[i].y);
+            let distance = ball.distanceFromPoint(balls[i].x, balls[i].y);
+            //collision med dvema krogoma
+            if(distance <= ball.r+balls[i].r){
+                let overlap = 0.5*(distance - ball.r - balls[i].r);
+                ball.x -= overlap * (ball.x - balls[i].x)/distance; //razstavimo dejanski razmak krogov v x in y koordinato -
+                ball.y -= overlap * (ball.y - balls[i].y)/distance; //npr. če bo x=0.2 bo y=0.8, skupaj bo vedno 1. Potem pomnožimo z
+                                                                    //razdaljo, za katero hočemo premakniti ball
+                distance = ball.distanceFromPoint(balls[i].x, balls[i].y);
+                balls[i].x += overlap * (ball.x - balls[i].x)/distance; 
+                balls[i].y += overlap * (ball.y - balls[i].y)/distance;
 
+
+                distance = ball.distanceFromPoint(balls[i].x, balls[i].y);
                 let xnormal = (ball.x - balls[i].x)/ball.distanceFromPoint(balls[i].x, balls[i].y); //vektor normale - ta gleda proti središču collided kroga
                 let ynormal = (ball.y - balls[i].y)/ball.distanceFromPoint(balls[i].x, balls[i].y);
                 
@@ -65,18 +86,38 @@ function mainLoop(){
                 let v2 = (2*ball.mass)/(ball.mass+balls[i].mass)*skalarNorm1
                 +(balls[i].mass-ball.mass)/(ball.mass+balls[i].mass)*skalarNorm2;
 
-                ball.vx = skalarTang1 * xtangent + xnormal * v1; //naša hitrost je skalarni produkt med novima vektorjema
+                ball.vx = skalarTang1 * xtangent + xnormal * v1; //naša hitrost je skalarni produkt novih vektorjev
                 ball.vy = skalarTang1 * ytangent + ynormal * v1;
                 balls[i].vx = skalarTang2 * xtangent + xnormal * v2;
                 balls[i].vy = skalarTang2 * ytangent + ynormal * v2;
+
             }
         }
+        //collision med zidovi in krogi
+        walls.forEach((wall) => {
+            let wallVec = new Point(wall.endx-wall.startx, wall.endy-wall.starty) //vektor med začetkom in koncem zidova
+            let ballVec = new Point(ball.x-wall.startx, ball.y-wall.starty); //vektor med krogom in začetkom kroga
+
+            //t je najbližja točka, ki se jo dotika krog, izražena v številu med 0 in 1 (to uporabimo potem za scalat vektor wallVec)
+            let length = wallVec.x*wallVec.x + wallVec.y*wallVec.y;
+            let t = Math.max(0, Math.min(length, (wallVec.x*ballVec.x)+(wallVec.y*ballVec.y)))/length; 
+            
+            let closestPoint = new Point(wall.startx + t*wallVec.x, wall.starty + t*wallVec.y);
+            let distance = distanceBetweenPoints(ball.x, ball.y, closestPoint.x, closestPoint.y);
+            
+            if(distance <= ball.r+wall.r){
+                
+            }
+        });
     });
     //paddle.drawImg("img/paddle.png");
     //paddle.draw("black");
     for(let i=0; i<balls.length; i++){
         //balls[i].draw("#fdad30");
         balls[i].draw("blue");
+    }
+    for(let i=0; i<walls.length; i++){
+        walls[i].draw("black", "black");
     }
     requestAnimationFrame(mainLoop);
 }
@@ -107,6 +148,12 @@ canvas.addEventListener("mousedown", (event) => {
         if(ball.distanceFromPoint(mouse.x, mouse.y) < ball.r)
             ball.selected = true;
     });
+    walls.forEach((wall) => {
+        if(distanceBetweenPoints(mouse.x, mouse.y, wall.startx, wall.starty) <= wall.r)
+            wall.startSelected = true;
+        if(distanceBetweenPoints(mouse.x, mouse.y, wall.endx, wall.endy) <= wall.r)
+            wall.endSelected = true;
+    });
 });
 canvas.addEventListener("mouseup", (event) => {
     mouseDown = false;
@@ -117,4 +164,12 @@ canvas.addEventListener("mouseup", (event) => {
         }
         ball.selected = false;
     });
+    walls.forEach((wall) => {
+        wall.startSelected = false;
+        wall.endSelected = false;
+    });
 });
+
+function distanceBetweenPoints(x1, y1, x2, y2){
+    return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+}
